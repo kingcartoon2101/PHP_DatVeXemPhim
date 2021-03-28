24a92b7e09372c1eb112c6a219158072757584d7
nmhieuCoder <80735296+nmhieuCoder@users.noreply.github.com>
2021-03-28 22:03:33 +0700

    delete index

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  define("fonts_Part","fonts/");
-  define("icon_Part","icon/");
-  define("css_Part","css/");
-  define("login_Part","contents/login/");
-  define("signup_Part","contents/signup/");
-  define("footer_Part","footer/");
-?>
