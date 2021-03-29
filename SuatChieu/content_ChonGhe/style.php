76309a3b80221a38ce3f8f5c295f3e783343e8c9
kingcartoon0445 <79097005+kingcartoon0445@users.noreply.github.com>
2021-03-29 23:02:18 +0700

    xếp ghế
    sắp ghế

---
+++ SuatChieu/content_ChonGhe/style.php
@@ -0,0 +1,157 @@
+/* ============================================ *
+ * Regular
+ * ============================================ */
+ <style>
+.booking-progress {
+    width: 100%;
+	border: 2px solid #d4d3c9;
+    #background-color: #F2F0E4;
+	display: block;
+}
+.booking-progress .main-content{   
+    white-space: nowrap;
+}
+.col-main .page-title {
+	margin-top: 0px;
+	text-align: center;
+	padding: 5px;
+	background: #231d1c;
+	width: 100%;
+}
+.col-main .page-title h1 {
+	text-transform: uppercase;
+	color: #fff;
+	line-height: 32px;
+	padding: 0;
+    margin: 0;
+}
+
+/* ============================================ *
+ * Top content
+ * ============================================ */
+.top-content {
+    width: 100%;
+    padding: 10px;
+    border-bottom: 2px solid #d4d3c9;
+    background: #fff1ce;
+	margin-bottom: 20px;
+	overflow: hidden;
+}
+.top-content .products-list {
+	width: 100%;
+    margin: 0px;
+}
+.top-content .products-list > li {
+    margin: 0px;
+	width: 100%;
+}
+.top-content .products-list .product-shop {
+    float: left;
+    padding: 0px;
+    width: 100%;
+}
+.top-content .products-list .product-shop * {
+	white-space: normal;
+}
+.countexpire {
+	display: none;
+}
+#countdown .cgvminutes, #countdown .cgvseconds{
+	font-weight: normal;
+}
+#countdown > div {
+	padding: 10px;
+    border-radius: 5px;
+    background: #fdfcf0;
+    display: inline-block;
+    text-align: center;
+}
+#countdown div > span {
+	padding: 10px;
+	border-radius: 5px;
+	background: #e71a0f;
+	display: inline-block;
+	width: 40px;
+	color: #fff;
+}
+.smalltext {
+	color:#999;
+}
+
+/* ============================================ *
+ * Main content
+ * ============================================ */
+.main-content {
+	overflow: hidden;
+}
+.progress {
+	width: 100%;
+}
+.progress > li {
+	width: 100%;
+}
+.main-content .h1 {
+	text-align: center;
+    width: 100%;
+	background: #bcbdc0;    
+	font-size: 16px;
+	line-height: 32px;
+	border-top: 2px solid #d4d3c9;
+    border-bottom: 2px solid #d4d3c9;
+}
+.ticketbox {
+	overflow-x: auto;
+	text-align: center;
+}
+.row {
+    margin: 2px;
+    font-size: 0;
+}
+.seat {
+	display: inline-block;
+    border: 1px solid transparent;
+    width: 22px;
+    height: 22px;
+    vertical-align: middle;
+    font-size: 7pt;
+    margin: 0 1px;
+    padding: 1px 0;
+}
+.seat.active {
+	cursor: pointer;
+}
+.seat.seat-disable {
+    background: #bbb;
+    border: 1px solid #bbb;
+    color: #fff;
+}
+.seat.seat-unavailable {
+    background: #bbb;
+    color: #fff;
+}
+.seat.seat-unavailable:before {
+    content: "X";
+    position: absolute;
+    font-size: 14px;
+}
+.seat-standard {
+    background: #ffbb99;
+}
+.seat-vipprime {
+	background:	 #ff884d;
+}
+.seat-sw {
+	background: #ff9933;									
+	color: #fff;
+
+}
+
+
+
+
+
+
+
+
+}
+</style>
